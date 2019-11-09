0f3eb8c5d537622989e451fdee4452a23ef5cd45
firozbangladesh <firoz.bangladesh.com>
2019-11-09 21:25:34 +0600

    Seedig Faker Data in Product and Review

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,11 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Product;
 use App\Model\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
-        //
+        'customer'  => $faker->name,
+        'review'    => $faker->paragraph,
+        'star'      => $faker->numberBetween(0,5),
+        'product_id'=> function(){
+            return Product::all()->random();
+        }
     ];
 });
